97905c0692660a4f6e382818c47b605fce714801
Dayan-02 <dayanherath8970@gmail.com>
2026-09-24 15:09:08 +0530

    fix: map sales identifiers for MySQL

--- Data/SalesDbContext.cs
+++ Data/SalesDbContext.cs
@@ -14,29 +14,32 @@ public sealed class SalesDbContext(DbContextOptions<SalesDbContext> options) : D
  protected override void OnModelCreating(ModelBuilder b)
  {
   var guidAsText = new GuidToStringConverter();
+  var isMySql = Database.ProviderName?.Contains("MySql", StringComparison.OrdinalIgnoreCase) == true;
+  var guidColumnType = isMySql ? "char(36)" : "TEXT";
 
   b.Entity<StockBatch>(entity =>
   {
-   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType("TEXT");
+   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType(guidColumnType);
    entity.ToTable(t => t.HasCheckConstraint("CK_StockBatch_RemainingQuantity", "RemainingQuantity >= 0"));
   });
   b.Entity<Sale>(entity =>
   {
-   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType("TEXT");
+   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType(guidColumnType);
+   entity.Property(x => x.IdempotencyKey).HasMaxLength(128);
    entity.HasIndex(x => x.IdempotencyKey).IsUnique();
    entity.HasMany(x => x.Items).WithOne(x => x.Sale).HasForeignKey(x => x.SaleId);
   });
   b.Entity<SaleItem>(entity =>
   {
-   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType("TEXT");
-   entity.Property(x => x.SaleId).HasConversion(guidAsText).HasColumnType("TEXT");
+   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType(guidColumnType);
+   entity.Property(x => x.SaleId).HasConversion(guidAsText).HasColumnType(guidColumnType);
    entity.HasMany(x => x.Allocations).WithOne(x => x.SaleItem).HasForeignKey(x => x.SaleItemId);
   });
   b.Entity<BatchAllocation>(entity =>
   {
-   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType("TEXT");
-   entity.Property(x => x.SaleItemId).HasConversion(guidAsText).HasColumnType("TEXT");
-   entity.Property(x => x.StockBatchId).HasConversion(guidAsText).HasColumnType("TEXT");
+   entity.Property(x => x.Id).HasConversion(guidAsText).HasColumnType(guidColumnType);
+   entity.Property(x => x.SaleItemId).HasConversion(guidAsText).HasColumnType(guidColumnType);
+   entity.Property(x => x.StockBatchId).HasConversion(guidAsText).HasColumnType(guidColumnType);
    entity.HasOne(x => x.StockBatch).WithMany().HasForeignKey(x => x.StockBatchId);
   });
  }
